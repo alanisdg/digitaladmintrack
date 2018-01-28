04509ded4e1ec200c0e1e7b853de963a924eebca
alanisdg <alanisdg@gmail.com>
2018-01-28 09:38:56 -0600

    portada

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,127 +1,110 @@
+
 <!DOCTYPE html>
 <html lang="en">
+
     <head>
+        <!-- Meta -->
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title id="title">Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-        #map-canvas {
-            opacity: 0.07;
-  height: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  z-index: 2;
-}
-            html, body {
-                background-color: #4a4a4a;
-
-                color: #9e9e9e;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-                    z-index: 1;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-                z-index: 3
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 40px;
-            }
-
-            .subtitle {
-                font-size: 20px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-            #img{
-                position: absolute;
-                opacity: .1;
-                width: 100%;
-                height: 100%;
-            }
-        </style>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <!-- SITE TITLE -->
+        <title>Ovi - Digital Agency Bootstrap Template</title>          
+        <!-- Latest Bootstrap min CSS -->
+        <link rel="stylesheet" href="/css/bootstrap.min.css">       
+        <!-- Google Font -->        
+        <link href="https://fonts.googleapis.com/css?family=Ubuntu:400,400i,500,500i,700,700i" rel="stylesheet">
+        <!-- themify-icons CSS -->
+        <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+        <!--- owl carousel Css-->
+        <link rel="stylesheet" href="/css/aos.css">           
+        <!-- Style CSS -->
+        <link rel="stylesheet" href="/css/style_home.css">     
+        <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
     </head>
-    <body>
-        @if (Route::has('login'))
-            <div class="top-right links">
-                <a href="{{ url('/login') }}">Login</a>
-            </div>
-        @endif
-        <div class="flex-center position-ref full-height">
+    
+    <body data-spy="scroll" data-offset="80">
 
-
-
-            <div class="content">
-                <div class="title m-b-md">
-                    <img width="100"src="images/map-marker-icon.png" alt="">
-                </div>
-                <div class="title m-b-md">
-                    DIGITAL ADMIN TRACK RASTREO SATELITAL
-                </div>
-                <div class="subtitle m-b-md">
-                    PROXIMAMENTE
-                </div>
+        <!-- START PRELOADER -->
+        <div class="preloader">
+            <div class="status">
+                <div class="status-mes"></div>
             </div>
         </div>
-        <div id='map-canvas'></div>
+        <!-- END PRELOADER -->      
+
+        <!-- START NAVBAR -->
+        <div class="navbar navbar-default navbar-fixed-top menu-top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3">
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                            <a href="index.html" class="navbar-brand"><img src="/images/logodat.png" alt="logo"></a>
+                        </div>
+                    </div>                  
+                    <div class="col-md-6">
+                        <div class="navbar-collapse collapse">
+                            <nav>
+                                <ul class="nav navbar-nav">
+                                    <li><a class="page-scroll" href="#about">Nosotros</a></li>
+                                    <li><a class="page-scroll" href="#service">Servicios</a></li>
+                                    <li><a class="page-scroll" href="#work">Aplicación</a></li>                         
+                                    <li><a class="page-scroll" href="#contact">Contacto</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                    <div class="menu_social">
+                            <ul>
+                                <!--
+                                <li><a href="#"><i class="fas 500px"></i></a></li>
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+                                <li><a href="#"><i class="fa fa-skype"></i></a></li>-->
+                            </ul>
+                        </div>
+                    </div>                  
+                </div><!--- END ROW -->
+            </div><!--- END CONTAINER -->
+        </div> 
+        <!-- END NAVBAR --> 
+
+        <!-- START HOME -->
+        <section data-stellar-background-ratio="0.3" id="home" class="home_parallax" style="background-image: url(/images/truck.jpg);  background-size:cover; background-position: center center;">         
+            <div class="container">
+                <div class="row">
+                  <div class="col-md-12 text-center">
+                    <div class="hero-text">
+                        <h1>Rastreo  Satelital<br>GPS</h1>
+                        <p></p>
+                        <a href="#contact" class="page-scroll btn btn-default btn-home-bg">Contactanos</a>                       
+                    </div>
+                  </div><!--- END COL -->         
+                </div><!--- END ROW -->
+            </div><!--- END CONTAINER -->
+        </section>
+        <!-- END  HOME DESIGN -->
+
+
+
+        <!-- Latest jQuery -->
+            <script src="/js/all.js"></script>
+            <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+
+
     </body>
 </html>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD5unzpQgOVgur7TbgtMpBdMKM7c7XGzWw" type="text/javascript"></script>
-
-<script type="text/javascript">
-  var mapOptions = {
-    zoom: 13,
-    center: new google.maps.LatLng(25.683488, -100.317341),
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  }
-  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-
-
-
-</script>
--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/welcome2', function () {
+    return view('welcome2');
+});
+
 Route::get('/clean_cron', 'CronsController@clean',function(){
     
 });
